8a2d7bcdadd0004f0ed460d09b082a3769da23c4
Danna <daeunlee0720@gmail.com>
2022-06-02 23:18:58 +0900

    feat: 디바이더 컴포넌트 퍼블리싱

--- src/components/common/Divider.tsx
+++ src/components/common/Divider.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import dividerImg from '@assets/divider.svg';
+import styled from '@emotion/styled';
+import Image from 'next/image';
+
 function Divider() {
-  return <div>Divider</div>;
+  return (
+    <Styled.Main>
+      <Image src={dividerImg} width="" alt="구분선" />
+    </Styled.Main>
+  );
 }
 
 export default Divider;
+
+const Styled = {
+  Main: styled.main`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+};
